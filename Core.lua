edd212338e52a9dfbdd82553e88a7ea3a69d4095
Javier <jarenasllorente@gmail.com>
2019-07-07 20:15:31 +0200

    Code cleaning

--- Core.lua
+++ Core.lua
@@ -1,47 +1,62 @@
-local AddOn = NameCopy
+_G.BINDING_HEADER_NAMECOPY = 'NameCopy Addon'
+_G.BINDING_NAME_NAMECOPY_COPY_ITEM = 'Copy item to clipboard'
 
-BINDING_HEADER_NAMECOPY = "NameCopy Addon"
-BINDING_NAME_NAMECOPY_COPY_ITEM = "Copy item to clipboard"
-
-function COPY_ITEM()
+function _G.NAME_COPY_ITEM()
     if _G.GameTooltip:NumLines() > 0 then
-        local text = _G["GameTooltipTextLeft1"]:GetText()
+        local text = _G['GameTooltipTextLeft1']:GetText()
         local d = {}
         d.text = text
-        StaticPopup_Show("ITEM_NAME_NAMECOPY", "", "", d)
+        _G.StaticPopup_Show('ITEM_NAME_NAMECOPY', '', '', d)
     end
 end
 
-local function addon_loaded() 
-    if NameCopyTooltipTextBool == nil then
-        NameCopyTooltipTextBool = true
-    else
-        _G.GameTooltip:HookScript("OnUpdate", function(self)
-            if NameCopyTooltipTextBool and NameCopyTooltipTextBool ~= nil then
-                local tooltipNumLines = _G.GameTooltip:NumLines()
-                local command, _, _ = GetBindingKey("NAMECOPY")
-                local tooltipLine
-                if not command then
-                    tooltipLine = ""
-                else
-                    tooltipLine = "Press |cFFFFFF4d" .. command ..
-                                      "|r to copy the name"
-                end
-                if tooltipNumLines > 0 and #tooltipLine > 1 then
-                    if (_G["GameTooltipTextLeft" .. tooltipNumLines]:GetText() ~=
-                        tooltipLine) then
-                        self:AddLine(tooltipLine, 0.56, 0.75, 0.99, 1)
-                        self:Show()
-                    end
+local db = _G.NameCopyTooltipTxteBool
+
+local function tooltip_update(self)
+    if db and db ~= nil then
+        local command, _, _ = _G.GetBindingKey('NAMECOPY')
+        if command then
+            local tooltipNumLines = _G.GameTooltip:NumLines()
+            local tooltipLine = 'Press |cFFFFFF4d' .. command .. '|r to copy the name'
+            local found = false
+            for line_count = 1, tooltipNumLines, 1 do
+                if (_G['GameTooltipTextLeft' .. line_count]:GetText() == tooltipLine) then
+                    found = true
+                    break
                 end
             end
-        end)
-    end    
+            if not found then
+                self:AddLine(tooltipLine, 0.56, 0.75, 0.99, 1)
+                self:Show()
+            end
+        end
+    end
+end
+
+local function hook_tooltips()
+    local obj = _G.EnumerateFrames()
+
+    while obj do
+        if not obj.copyHook then
+            if obj:IsObjectType('GameTooltip') then
+                obj.copyHook = true
+                obj:HookScript('OnUpdate', tooltip_update)
+            end
+        end
+        obj = _G.EnumerateFrames(obj)
+    end
+end
+
+local function addon_loaded()
+    if db == nil then
+        db = true
+    end
+    hook_tooltips()
 end
 
 --save & remove a binding
 local function SafeSetBinding(key, action)
-    if key == "" then
+    if key == '' then
         local oldkey = _G.GetBindingKey(action)
         if oldkey then
             _G.SetBinding(oldkey, nil)
@@ -55,79 +70,75 @@ end
 --set a default binding if no one has it
 local function SetDefaultBinding(key, action)
     --get our binding
-    local ourkey1,ourkey2 = _G.GetBindingKey(action);
+    local ourkey1, ourkey2 = _G.GetBindingKey(action)
 
     --if we dont have it
-    if (ourkey1==nil) and (ourkey2==nil) then
-
+    if (ourkey1 == nil) and (ourkey2 == nil) then
         --get possible action for this binding since CTRL-C or CTRL-SHIFT-C look the same
-        local possibleAction = _G.GetBindingByKey(key);
+        local possibleAction = _G.GetBindingByKey(key)
 
         --by default we could set this binding
-        local okToSet = true;
+        local okToSet = true
 
         --if any action
         if possibleAction then
-
             --get the action keys
-            local key1,key2 = _G.GetBindingKey(possibleAction);
+            local key1, key2 = _G.GetBindingKey(possibleAction)
 
             --if any key match our key
             if (key1 == key) or (key2 == key) then
-                okToSet = false;
+                okToSet = false
             end
-
         end
 
         --if ok to set
         if okToSet then
-            SafeSetBinding(key,action);
+            SafeSetBinding(key, action)
         end
-
     end
 end
 
 local function entering_world()
-    SetDefaultBinding("CTRL-C", "NAMECOPY")
+    SetDefaultBinding('CTRL-C', 'NAMECOPY')
 end
 
-local frame = CreateFrame("FRAME")
-frame:RegisterEvent("ADDON_LOADED")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+local frame = _G.CreateFrame('FRAME')
+frame:RegisterEvent('ADDON_LOADED')
+frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 
 local function eventHandler(_, event, arg1)
-    if event == "ADDON_LOADED" then
-        if  arg1 == "NameCopy" then
+    if event == 'ADDON_LOADED' then
+        if arg1 == 'NameCopy' then
             addon_loaded()
         end
-    elseif event == "PLAYER_ENTERING_WORLD" then
+    elseif event == 'PLAYER_ENTERING_WORLD' then
         entering_world()
     end
 end
 
-frame:SetScript("OnEvent", eventHandler)
+frame:SetScript('OnEvent', eventHandler)
 
-StaticPopupDialogs["ITEM_NAME_NAMECOPY"] =
-    {
-        text = "Copy the name of the Item/NPC/Object",
-        button1 = "Close",
-        OnAccept = function() end,
-        timeout = 0,
-        whileDead = true,
-        hideOnEscape = true,
-        preferredIndex = 3,
-        OnShow = function(self, data)
-            self.editBox:SetText(data.text)
-            self.editBox:HighlightText()
-        end,
-        hasEditBox = true
-    }
+_G.StaticPopupDialogs['ITEM_NAME_NAMECOPY'] = {
+    text = 'Copy the name of the Item/NPC/Object',
+    button1 = 'Close',
+    OnAccept = function()
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3,
+    OnShow = function(self, data)
+        self.editBox:SetText(data.text)
+        self.editBox:HighlightText()
+    end,
+    hasEditBox = true
+}
 
-SLASH_NAMECOPY1 = "/nc"
-function SlashCmdList.NAMECOPY(msg)
-    NameCopyTooltipTextBool = not NameCopyTooltipTextBool
-    if NameCopyTooltipTextBool then
-        print("|cFF00FF96NameCopy|r will add a line to tooltips")
+_G.SLASH_NAMECOPY1 = '/nc'
+function _G.SlashCmdList.NAMECOPY(_)
+    db = not db
+    if db then
+        print('|cFF00FF96NameCopy|r will add a line to tooltips')
     else
         print("|cFF00FF96NameCopy|r won't add a line to tooltips anymore")
     end
